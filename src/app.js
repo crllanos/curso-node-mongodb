128eebda3607068b8e4ff5b61d14dccb1acc9a1f
cristian llanos <cristian.llanos@gmail.com>
2019-04-06 23:35:20 -0300

    Conexión MongoDB. Routes.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,25 @@
-const app = require('express')();
+// imports
+const app 		= require('express')(); // npm run dev
+const morgan 	= require('morgan');
+const routes 	= require('./routes/index');
+const mongoose 	= require('mongoose'); // sudo mongod --dbpath ./data/db --port 27017 
 
-app.listen(8080, () => {
-	console.log('escuchando por el puerto 8080...');
-	console.log('Hola mundo');
+// settings
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.set('PORT', process.env.PORT || 8080);
+
+// conexion DB
+mongoose.connect('mongodb://localhost/crud-express-mongodb')
+		.then(db => console.log('Conexion db OK!'))
+		.catch(pifia => console.log(pifia))
+
+// middlewares
+app.use(morgan('dev'));
+app.use('/', routes);
+
+
+// server
+app.listen(app.get('PORT'), () => {
+	console.log('escuchando por el puerto '+ app.get('PORT') +'...');
 });
