f451db89377513abcd14d7a7b26b8d06fa83ecc4
cristian llanos <cristian.llanos@gmail.com>
2019-04-07 11:46:55 -0400

    Mensaje hola mundo, modelo de prueba.

---
+++ src/models/task.js
@@ -0,0 +1,12 @@
+const Schema = require('mongoose').Schema;
+
+const TaskSchema = Schema({
+  title: String,
+  description: String,
+  status: {
+    type: Boolean,
+    default: false
+  }
+});
+
+module.exports = mongoose.model('tasks', TaskSchema);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,2 +1,7 @@
 const router = require('express').Router();
+
+router.get('/', function(req, res){
+	res.send("Hola mundo");
+});
+
 module.exports = router;
